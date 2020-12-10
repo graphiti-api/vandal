16fff4c4d8f76a1ac1ce9163657bd31e5fe2136e
Xheladin Popaj <xheladin.popaj@metoda.com>
2020-12-10 16:20:49 +0100

    store encrypted token locally

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,6 +1,33 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import createPersistedState from 'vuex-persistedstate';
+import { MutationTree } from "vuex"
+import SecureLS from 'secure-ls';
+const ls = new SecureLS({ isCompression: false })
 
 Vue.use(Vuex);
 
-export default new Vuex.Store({});
+class State {
+  token: string | null = null;
+}
+
+const mutations = <MutationTree<State>>{
+  setToken (state, payload) {
+    state.token = payload;
+  }
+};
+
+export default new Vuex.Store({
+  state: new State(),
+  mutations: mutations,
+  plugins: [
+    createPersistedState({
+      storage: {
+        getItem: key => ls.get(key),
+        setItem: (key, value) => ls.set(key, value),
+        removeItem: key => ls.remove(key)
+      }
+    })
+  ],
+})
+
